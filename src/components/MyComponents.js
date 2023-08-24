ed6082883971fff0ad40830b8b22a8c7956fb128
HaoLong4935 <longsachse@gmail.com>
2023-08-24 21:30:28 +0700

    Section4_Video 16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponents extends React.Component{
+    //Nó dùng cú pháp JSX : Cho phép có thể viết code js bên trong code của html
+    render(){
+        return (
+            <div>
+                <h1>ALLO</h1>
+            </div>
+        );
+    }
+}
+
+export default MyComponents
