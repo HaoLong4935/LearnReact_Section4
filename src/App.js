95552cde32896ada4ac65aa61292b4d64b17b061
HaoLong4935 <longsachse@gmail.com>
2023-08-25 10:31:50 +0700

    Section4_Video17

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponents from './components/MyComponents';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import MyComponents from "./components/MyComponents";
+//Đầu tiên để viết component thì phải khai báo react
+import React from "react";
 
-  return (
-    <div>
-        Hello la anh Long
-        <MyComponents></MyComponents>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hảo Long best yasua
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+//Định nghĩa class App , nhớ là phải thừa kế
+class App extends React.Component {
+  //Dùng hàm render để xuất hình giao diện
+  render() {
+    return (
+      <div>
+        <div>
+          Hello la anh Long
+          <MyComponents></MyComponents>
+        </div>
+      </div>
+    );
+  }
 }
+
+// const App = () => {
+//   return (
+
+//   );
+// }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hảo Long best yasua
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-class MyComponents extends React.Component{
-    //Nó dùng cú pháp JSX : Cho phép có thể viết code js bên trong code của html
-    render(){
-        return (
-            <div>
-                <h1>ALLO</h1>
-            </div>
-        );
-    }
+class MyComponents extends React.Component {
+  //Nó dùng cú pháp JSX : Cho phép có thể viết code js bên trong code của html
+
+  //Định nghĩa state là một đối tượng có các thuộc tính
+  //State là thằng kiểm soát data của component , nên khi data thay đổi , render giao diện
+  //cũng sẽ thay đổi => Giao diện ĐỘNG
+  state = {
+    name: "Hao Long",
+    address: "Da Nang",
+    age: 26,
+  };
+  render() {
+    return (
+      <div>
+        <h1>My name is {this.state.name}</h1>
+      </div>
+    );
+  }
 }
 
-export default MyComponents
+export default MyComponents;
