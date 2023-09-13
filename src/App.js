d94590eda455b337354bab2d8fd9455eda92abbb
HaoLong4935 <longsachse@gmail.com>
2023-09-13 22:22:35 +0700

    Section9_Video47

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponents from "./components/MyComponents";
 //Đầu tiên để viết component thì phải khai báo react
 import React from "react";
+import Header from './components/Header/Header'
 
-//Định nghĩa class App , nhớ là phải thừa kế
-class App extends React.Component {
-  //Dùng hàm render để xuất hình giao diện
-  render() {
-    return (
-      <div className="app-container">
-        <div>
-          Hello la anh Long
-          <MyComponents></MyComponents>
-        </div>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header></Header>
+    </div>
+  )
 }
 
 export default App;
