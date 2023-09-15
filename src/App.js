dc276101c0fa5be560cfeec1430c6d1f6ac46d18
HaoLong4935 <longsachse@gmail.com>
2023-09-15 13:54:31 +0700

    Section9 Video 47

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import "./App.scss";
 //Đầu tiên để viết component thì phải khai báo react
 import React from "react";
 import Header from './components/Header/Header'
+//Thư viện chuyển trang Link 
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
       <Header></Header>
+      <div>TestLink</div>
+      <button>
+        <Link to="/user">Go to user</Link>
+      </button>
+      <button>
+        <Link to="/admin">Go to admin</Link>
+      </button>
     </div>
   )
 }
