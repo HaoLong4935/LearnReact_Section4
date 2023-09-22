15f57374b2f5aaa59cbefdaacc9db206ce456f4c
HaoLong4935 <longsachse@gmail.com>
2023-09-22 16:00:00 +0700

    Section9 Video 51

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,25 +2,26 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary" bg="light" data-bs-theme="light">
             <Container>
-                <Navbar.Brand href="#home">ANHLONELY</Navbar.Brand>
+                {/* <Navbar.Brand href="/">ANHLONELY</Navbar.Brand> */}
+                <NavLink to="/" className='navbar-brand'>HẢO LONG</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to="/" className='nav-link'>Home</Link>
-                        <Link to="/user" className='nav-link'>User</Link>
-                        <Link to="/admin" className='nav-link'>Admin</Link>
+                        <NavLink to="/" className='nav-link'>Home</NavLink>
+                        <NavLink to="/user" className='nav-link'>User</NavLink>
+                        <NavLink to="/admin" className='nav-link'>Admin</NavLink>
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">User Info</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
+                            <NavDropdown.Item>Login</NavDropdown.Item>
+                            <NavDropdown.Item>User Info</NavDropdown.Item>
+                            <NavDropdown.Item>Logout</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div className="homepage-container">
+            HOME PAGE CONTAINT
+        </div>
+    )
+}
+
+export default HomePage
